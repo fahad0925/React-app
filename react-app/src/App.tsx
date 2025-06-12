00431e8f1c37c42ed1c28f734cb11d64d81bd02c
fahad0925 <fahadqwerty777@gmail.com>
2025-06-12 01:31:47 -0700

    changes

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,52 +1,15 @@
-import { useState } from "react";
 import ListGroup from "./components/ListGroup";
+
 function App() {
   let items = ["Pakistan", "China", "Afganistan", "Indonesia"];
-  const [selectedText, setSelectedText] = useState(-1);
   return (
-    <>
-      <h1>List</h1>
-      {items.length === 0 && <p>No List Found</p>}
-      <ul className="list-group">
-        {items.map((item, index) => (
-          <li
-            className={
-              selectedText === index
-                ? "list-group-item  active"
-                : "list-group-items"
-            }
-            key={item}
-            onClick={() => {
-              setSelectedText(index);
-            }}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
-      {/* <button
-        onClick={() => setValue(value + 1)}
-        type="button"
-        className="btn btn-primary"
-      >
-        Increase
-      </button>
-      <button
-        onClick={() => setValue(value - 1)}
-        type="button"
-        className="btn btn-secondary"
-      >
-        Decrease
-      </button>
-      <button
-        onClick={() => setValue(0)}
-        type="button"
-        className="btn btn-danger"
-      >
-        reset
-      </button>
-      {value} */}
-    </>
+    <div>
+      <ListGroup
+        items={items}
+        heading="Countries"
+        onSelectItem={(item: string) => console.log(item)}
+      />
+    </div>
   );
 }
 export default App;
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,4 +1,58 @@
-function ListGroup(){
-    return <h1>GroupList</h1>
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
 }
-export default ListGroup
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedText, setSelectedText] = useState(-1);
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No List Found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedText === index
+                ? "list-group-item  active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedText(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+      {/* <button
+        onClick={() => setValue(value + 1)}
+        type="button"
+        className="btn btn-primary"
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => setValue(value - 1)}
+        type="button"
+        className="btn btn-secondary"
+      >
+        Decrease
+      </button>
+      <button
+        onClick={() => setValue(0)}
+        type="button"
+        className="btn btn-danger"
+      >
+        reset
+      </button>
+      {value} */}
+    </>
+  );
+}
+export default ListGroup;
